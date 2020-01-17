579a9cb1585795aa583f700b3fcab26f1c365d70
AShephardGD <Koleg02@mail.ru>
2020-01-17 22:59:31 +0300

    ERROR NUM 1

--- app/src/main/java/com/example/rockbee/MainActivity.java
+++ app/src/main/java/com/example/rockbee/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.rockbee;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    File directory = new File("");
+
+    File[] b = File.listRoots();
+    String a = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        for(File root: b)  {
+            a += "          " + root.getAbsolutePath() + "\n";
+        }
+        a += "       " + b[0].isDirectory();
+        a += "       " + b[0].list().length;
+        TextView tw = findViewById(R.id.tw);
+        tw.setText(a);
     }
+
 }
